6a15350efd576025e497e85076a06d4a8115fa65
Benson Doan <doanbenson@gmail.com>
2024-01-06 18:26:18 -0800

    fixed stuff

--- home.js
+++ home.js
@@ -1,15 +1,14 @@
-var checkboxes = document.querySelectorAll('.task-checkbox');
+var tasks = document.querySelectorAll(".task-checkbox");
 
-    // Add a click event listener to each checkbox
-    checkboxes.forEach(function(checkbox) {
-        checkbox.addEventListener('click', function() {
+    tasks.forEach(function(task) {
+        task.addEventListener('click', function() {
             // Check if the checkbox is checked
-            if (checkbox.checked) {
+            if (task.checked) {
                 // If checked, make the parent task-item disappear
-                checkbox.closest('.task-item').style.display = 'none';
-            } else {
-                // If unchecked, make the parent task-item visible
-                checkbox.closest('.task-item').style.display = 'block'; // You can use "flex" or any other display property that fits your layout
+                task.closest('.task-item').style.textDecoration = "line-through";
+            } 
+            else {
+                task.closest('.task-item').style.textDecoration = "none";
             }
         });
     });
